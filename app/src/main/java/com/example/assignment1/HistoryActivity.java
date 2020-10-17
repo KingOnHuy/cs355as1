c59ee5dc390abf88d3c643a68369e098a9a74a33
KingOnHuy <kingponpawit@gmail.com>
2020-10-17 23:36:32 +0700

    Add new HistoryActivity, Color value, Read MNC

---
+++ app/src/main/java/com/example/assignment1/HistoryActivity.java
@@ -0,0 +1,13 @@
+package com.example.assignment1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HistoryActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+    }
+}
--- app/src/main/java/com/example/assignment1/MainActivity.java
+++ app/src/main/java/com/example/assignment1/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.assignment1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.text.TextUtils;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,13 +15,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final TextView txtView=(TextView)findViewById(R.id.assign);
+        final TextView txtAppName = (TextView)findViewById(R.id.assign);
         final TextView assign = (TextView)findViewById(R.id.sales);
+
+        TelephonyManager tel = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        String networkOperator = tel.getNetworkOperator();
+        if (!TextUtils.isEmpty(networkOperator)) {
+            int mcc = Integer.parseInt(networkOperator.substring(0, 3));
+            int mnc = Integer.parseInt(networkOperator.substring(3));
+
+            txtAppName.setText(txtAppName.getText() + " MCC-" + mcc + " MNC-" + mnc + " RAW-" + networkOperator);
+        }
     }
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
-        final TextView txtView=(TextView)findViewById(R.id.assign);
+        final TextView txtAppName = (TextView)findViewById(R.id.assign);
         final TextView assign = (TextView)findViewById(R.id.sales);
+
+//        final ImageButton historyBtn = (ImageButton)findViewById(R.id.historyBtn);
+//        historyBtn.setColorFilter(newConfig.mnc);
+
+        txtAppName.setText(txtAppName.getText() + " - " + newConfig.mnc);
 //        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
 //            txtView.setText(txtView.getText() + " landscape");
 //            Toast.makeText(this, "landscape", Toast.LENGTH_SHORT).show();
