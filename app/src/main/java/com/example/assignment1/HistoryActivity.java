b1dab9322bc8c63da7c4ef5044f06198cee4cb54
KingOnHuy <kingponpawit@gmail.com>
2020-10-18 23:47:13 +0700

    Create DatabaseHelper and DBManager

--- app/src/main/java/com/example/assignment1/HistoryActivity.java
+++ app/src/main/java/com/example/assignment1/HistoryActivity.java
@@ -2,14 +2,22 @@ package com.example.assignment1;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HistoryActivity extends AppCompatActivity {
+    private DBManager dbManager;
+    private ListView listView;
+    private SimpleCursorAdapter adapter;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("History");
         setContentView(R.layout.activity_history);
+
     }
 }
